b4524f51a102f4f24de13e3eebb08264f22f8d93
zzyssg <zy1704528@buaa.edu.cn>
2024-05-18 22:47:23 +0800

    finish mybatis-generator

---
+++ mall-test/src/main/java/com/zzy/mall/test/mbg/CommentGenerator.java
@@ -0,0 +1,34 @@
+package com.zzy.mall.test.mbg;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName CommentGenerator
+ * @Author ZZy
+ * @Date 2024/5/18 22:02
+ * @Description
+ * @Version 1.0
+ */
+public class CommentGenerator {
+    public static void main(String[] args) throws XMLParserException, IOException, InvalidConfigurationException, SQLException, InterruptedException {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        InputStream is = CommentGenerator.class.getResourceAsStream("/generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(is);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
